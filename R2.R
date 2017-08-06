2ae8e0934b463d7bc338f7445027e0945e0b45e2
xiangjiawei <xiangjiawei@my.com>
2017-08-06 22:53:50 +0800

    R2.R

--- R2.R
+++ R2.R
@@ -1,24 +1,34 @@
+##the first function, makeVector creates a special "vector", which is really a list containing a function to
+
+##set the value of the matrix
+##get the value of the matrix
+##set the value of the inverse
+##get the value of the inverse
 makeCacheMatrix <- function(x = numeric()) {
-  m <- NULL
-  set <- function(y) {
+  i <- NULL
+  set <- function(y) { ##function set the vaule of the matrix
     x <<- y
-    m <<- NULL
+    i <<- NULL
   }
-  get <- function() x
-  setinverse <- function(inverse) i <<- inverse
-  getinverse <- function() i
+  get <- function() x  ##return the value of the matrix
+  setinverse <- function(inverse) i <<- inverse ##set the inverse of the matrix
+  getinverse <- function() i  ##get the inverse of the matrix
   list(set = set, get = get,
        setinverse = setminverse,
        getinverse = getinverse)
 }
+
+## the second function is to find whether there have stored the inverse of the matrix, if so, use the value that calcuted previously. 
+##if not, calculate the inverse value of the matrix.
+
 cacheinverse <- function(x, ...) {
   i <- x$getinverse()
   if(!is.null(i)) {
     message("getting cached data")
-    return(i)
+    return(i)  ##matrix that is the inverse of i will be returned
   }
   data <- x$get()
   i <- inverse(data, ...)
   x$setinverse(i)
-  i
+  i ##return the matrix
 }
