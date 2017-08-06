34d8e8a91b6e45c053fb0a77f100fae75b6288fe
xiangjiawei <xiangjiawei@my.com>
2017-08-06 22:04:30 +0800

    Add files via upload
    R-Programming Assignment2

---
+++ R2.R
@@ -0,0 +1,24 @@
+makeCacheMatrix <- function(x = numeric()) {
+  m <- NULL
+  set <- function(y) {
+    x <<- y
+    m <<- NULL
+  }
+  get <- function() x
+  setinverse <- function(inverse) i <<- inverse
+  getinverse <- function() i
+  list(set = set, get = get,
+       setinverse = setminverse,
+       getinverse = getinverse)
+}
+cacheinverse <- function(x, ...) {
+  i <- x$getinverse()
+  if(!is.null(i)) {
+    message("getting cached data")
+    return(i)
+  }
+  data <- x$get()
+  i <- inverse(data, ...)
+  x$setinverse(i)
+  i
+}
